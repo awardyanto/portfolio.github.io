df3ef7c7efce37ca06d3545e024e6cdd6b9f186d
alfian wardyanto <118360326+awardyanto@users.noreply.github.com>
2025-08-22 17:55:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-// Modal
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
+// Modal handling
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("myBtn");
+const span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-span.onclick = function() {
-  modal.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+btn.onclick = () => { modal.style.display = "block"; };
+span.onclick = () => { modal.style.display = "none"; };
+window.onclick = (e) => { if (e.target == modal) modal.style.display = "none"; };
